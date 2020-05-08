633c92005d2194c53f6b1c0089ff4024207a472b
Hisa111 <hisathebiggest@gmail.com>
2020-05-08 15:39:36 +0900

    added admin layouts, and user information

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/categories', 'AdminController@categories')->name('admin.categories');
+
+Route::get('/admin/answers', 'AdminController@answers')->name('admin.answers');
